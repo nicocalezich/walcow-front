4bb487aa5dabdd07da8d5cd486f9ee74d914f6dc
Matías Olivera <matibjolivera@gmail.com>
2021-06-24 19:49:27 -0300

    mejoras en perfil

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -20,7 +20,8 @@
     </div>
     <div class="container-fluid general balance">
       <p class="text-black-50">Balance aproximado</p>
-      <h2>$1800.00</h2>
+      <h2>${{ totalInUSD }}</h2>
+      <pre>≈ {{ totalInUSD / bitcoinPrice }} BTC</pre>
     </div>
     <div class="container-fluid general balance">
       <p class="text-black-50">Balance de criptomonedas</p>
@@ -29,43 +30,24 @@
         <tr>
           <th scope="col">Moneda</th>
           <th scope="col">Cantidad</th>
-          <th scope="col">Total USD</th>
+          <th scope="col">Total</th>
           <th scope="col">Acciones</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
+        <tr v-for="(w, i) in wallets" :key="i">
           <td>
             <div class="cripto">
-              <img src="../../assets/eth.png" style="margin-right: 5px; float: left" alt="">
+              <img :src="w.token.logo" style="margin-right: 5px; float: left" alt="">
               <div>
-                <span>ETH</span>
+                <span>{{ w.token.code }}</span>
                 <br>
-                <a href="" class="cripto-link">Ethereum</a>
+                <a href="" class="cripto-link">{{ w.token.name }}</a>
               </div>
             </div>
           </td>
-          <td>0.50</td>
-          <td>$1500</td>
-          <td>
-            <a href="#" class="actions">Comprar</a>
-            <a href="#" class="actions">Depositar</a>
-            <a href="#" class="actions">Retirar</a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div class="cripto">
-              <img src="../../assets/btc_png.png" style="margin-right: 5px; float: left" alt="">
-              <div>
-                <span>BTC</span>
-                <br>
-                <a href="" class="cripto-link">Bitcoin</a>
-              </div>
-            </div>
-          </td>
-          <td>0.2</td>
-          <td>$25000</td>
+          <td>{{ w.quantity }} {{ w.token.code }}</td>
+          <td>${{ w.quantity * w.token.price }}</td>
           <td>
             <a href="#" class="actions">Comprar</a>
             <a href="#" class="actions">Depositar</a>
@@ -83,14 +65,78 @@
 <script>
 
 import Navbar from '../Navbar.vue'
+
 export default {
-  name: "Profile",
+  name: "Home",
   components: {
     Navbar
   },
   mounted() {
-    this.$store.dispatch('checkAccess')
+    //this.$store.dispatch('checkAccess')
+    this.setBitcoinPrice()
+    this.calculateTotal()
   },
+  props: {
+    user: {
+      token: "416c3a405ba1c59b0a3366ab31e02fe21fd1a047",
+      fiat: 1000,
+      username: "nicocalezich",
+      email: "ncalezich@gmail.com",
+      firstname: "Nicolás",
+      lastname: "Calezich",
+      wallets: [
+        "60ca9cd60d840344243c537b",
+        "60ca9cd60d840344243c537c"
+      ]
+    },
+  },
+  methods: {
+    async setBitcoinPrice() {
+      let result = await fetch('https://walcow-api.herokuapp.com/api/tokens/price/bitcoin')
+      let json = await result.json()
+      console.log(json)
+      this.bitcoinPrice = json.result.token.price
+    },
+    calculateTotal() {
+      this.wallets.map(w => {
+        this.totalInUSD += w.quantity * w.token.price
+      })
+      this.totalInBTC = this.totalInUSD / this.bitcoinPrice
+    }
+  },
+  data() {
+    return {
+      bitcoinPrice: 0,
+      wallets: [
+        {
+          "id": "60ca9cd60d840344243c537b",
+          "token": {
+            "id": "1289031290319023",
+            "code": "BTC",
+            "name": "Bitcoin",
+            "logo": require("../../assets/btc_png.png"),
+            "btcRelation": 1,
+            "price": 25000
+          },
+          "quantity": 10
+        },
+        {
+          "id": "60ca9cd60d840344243c537c",
+          "token": {
+            "id": "1289031290319023",
+            "code": "ETH",
+            "name": "Ethereum",
+            "logo": require("../../assets/eth.png"),
+            "btcRelation": 0.0647,
+            "price": 4000
+          },
+          "quantity": 40
+        }
+      ],
+      totalInUSD: 0,
+      totalInBTC: 0
+    }
+  }
 }
 </script>
 
