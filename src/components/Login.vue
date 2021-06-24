a3b618ad92f739e405f1f577c56e0219a8700307
Matías Olivera <matibjolivera@gmail.com>
2021-06-24 20:10:42 -0300

    guardo token after login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,12 +7,12 @@
         <validate tag="div">
           <label for="user">Username</label>
           <input
-            name="username"
-            id="username"
-            type="text"
-            placeholder="Enter username"
-            v-model.trim="formData.username"
-            required
+              name="username"
+              id="username"
+              type="text"
+              placeholder="Enter username"
+              v-model.trim="formData.username"
+              required
           />
           <field-messages name="username" show="$dirty">
             <div slot="required" class="alert alert-danger error">
@@ -25,12 +25,12 @@
         <validate tag="div">
           <label for="password">Password</label>
           <input
-            name="password"
-            id="password"
-            type="password"
-            placeholder="Enter password"
-            v-model.trim="formData.password"
-            required
+              name="password"
+              id="password"
+              type="password"
+              placeholder="Enter password"
+              v-model.trim="formData.password"
+              required
           />
           <field-messages name="password" show="$dirty">
             <div slot="required" class="alert alert-danger error">
@@ -40,21 +40,21 @@
         </validate>
         <!-- btn ingresar -->
         <button
-          type="submit"
-          :disabled="formState.$invalid"
-          class="btn btn-light"
+            type="submit"
+            :disabled="formState.$invalid"
+            class="btn btn-light"
         >
           <b>Log in</b>
         </button>
         <br>
-          <div class="redirect">     
-            <router-link to="/access/register"><a href="#">Don't have an account? Sign up here</a></router-link>                          
-          </div>
-          <div class="redirect">     
-            <router-link class="forgot-pass" to="/access/forgot"><a href="#"><i>I forgot my password</i></a></router-link>                          
-          </div>
+        <div class="redirect">
+          <router-link to="/access/register"><a href="#">Don't have an account? Sign up here</a></router-link>
+        </div>
+        <div class="redirect">
+          <router-link class="forgot-pass" to="/access/forgot"><a href="#"><i>I forgot my password</i></a></router-link>
+        </div>
         <div class="alert alert-danger error mt-3 invalid-credentials" v-if="invalidCredentials">
-          {{this.errorMessage}}
+          {{ this.errorMessage }}
         </div>
       </vue-form>
     </div>
@@ -62,53 +62,53 @@
 </template>
 
 <script lang="js">
-  import axios from "axios";
-  export default  {
-    name: 'src-components-login',
-    props: [],
-    mounted () {
+import axios from "axios";
 
-    },
-    data () {
-      return {
-        formData: this.getInicialData(),
-        formState: {},
-        invalidCredentials: false,
-        errorMessage: ''
-      }
-    },
-    methods: {
-       getInicialData(){
-        return{
-          username: '',
-          password: '', 
-        }
-      },
-       enviar(){
-        let credentials = {
-          username: this.formData.username,
-          password: this.formData.password
-        }
-        try {
-          axios.post(process.env.url + "/api/users/login", credentials)
-          .then(res =>{
-            this.invalidCredentials = !res.data.canLogin;
-            this.errorMessage = res.data.message
-            if(res.data.canLogin){
-             this.$store.dispatch('access', res.data.canLogin)
-             this.$router.push('/home')
-            }
-          })
-        } catch (error) {
-          console.log(error)
-        }
-        this.formData = this.getInicialData()
-        this.formState._reset()
-      }
-    },
-    computed: {
+export default {
+  name: 'src-components-login',
+  props: [],
+  mounted() {
 
+  },
+  data() {
+    return {
+      formData: this.getInicialData(),
+      formState: {},
+      invalidCredentials: false,
+      errorMessage: ''
     }
+  },
+  methods: {
+    getInicialData() {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+    enviar() {
+      let credentials = {
+        username: this.formData.username,
+        password: this.formData.password
+      }
+      try {
+        axios.post("https://walcow-api.herokuapp.com/api/users/login", credentials)
+            .then(res => {
+              this.invalidCredentials = !res.data.canLogin;
+              this.errorMessage = res.data.message
+              if (res.data.canLogin) {
+                localStorage.setItem('token', res.data.user.token)
+                this.$store.dispatch('access', res.data.canLogin)
+                this.$router.push('/home')
+              }
+            })
+      } catch (error) {
+        console.log(error)
+      }
+      this.formData = this.getInicialData()
+      this.formState._reset()
+    }
+  },
+  computed: {}
 }
 </script>
 
@@ -158,20 +158,20 @@
   padding: 0px 20px;
 }
 
-.redirect{
+.redirect {
   text-align: center;
 }
 
-.redirect :hover{
+.redirect :hover {
   border-bottom: 1px solid;
 }
 
-.redirect a{
+.redirect a {
   text-decoration: none;
   color: rgb(255, 255, 255);
 }
 
-.invalid-credentials{
+.invalid-credentials {
   font-size: 21px;
   text-align: center;
 }
@@ -184,7 +184,7 @@
   margin-top: -2.5rem;
 }
 
-.forgot-pass{
+.forgot-pass {
   font-size: 13px;
 }
 
